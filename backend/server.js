6912c44a77f01461a4fa2d2aafe64861b9bc6edf
Daveedu <daveeduk@aditya.ac.in>
2026-01-07 20:30:04 +0530

    in production

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const uploadRoutes = require('./routes/uploadRoutes')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Middleware
 const allowedOrigins = [
   // 'http://localhost:5173',           // Vite dev server
